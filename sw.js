e763da7799764523a6d259edd577252c0060286b
faurpista <92080435+faurpista@users.noreply.github.com>
2026-07-28 21:37:09 +0200

    Increment cache version to v13
    Updated the cache version number from v12 to v13.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Írd át a verziószámot (pl. v2, v3), ha módosítasz a kódodon!
-const CACHE_NAME = 'prompt-akademia-v12';
+const CACHE_NAME = 'prompt-akademia-v13';
 
 // Telepítéskor az új fájlokat gyorsítótárazzuk
 self.addEventListener('install', (event) => {
