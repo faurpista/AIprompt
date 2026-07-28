82d33a1a195a5d4986e1a3de7f9a980c12c564f5
faurpista <92080435+faurpista@users.noreply.github.com>
2026-07-28 21:47:38 +0200

    Change cache name from v13 to v14
    Updated cache version number to v14.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Írd át a verziószámot (pl. v2, v3), ha módosítasz a kódodon!
-const CACHE_NAME = 'prompt-akademia-v13';
+const CACHE_NAME = 'prompt-akademia-v14';
 
 // Telepítéskor az új fájlokat gyorsítótárazzuk
 self.addEventListener('install', (event) => {
